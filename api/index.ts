ce0850ceadcd5436b6c806bf388fdd0f75606768
imnaeem <ceinfo.pk@gmail.com>
2025-09-09 07:49:24 +0500

    Update path

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getServer } from '../src/app.factory';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const server = await getServer();
-  return server(req, res);
-}
